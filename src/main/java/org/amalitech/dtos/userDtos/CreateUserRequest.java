a39cd8403772e9487d5f8cb8aeddcf4d1f9695d8
Jacob Quaye <kofimave@gmail.com>
2026-02-09 07:16:39 +0000

    feat: CreateUserRequest to create a user from API endpoint

---
+++ src/main/java/org/amalitech/dtos/userDtos/CreateUserRequest.java
@@ -0,0 +1,21 @@
+package org.amalitech.dtos.userDtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import org.amalitech.interfaces.ValidPassword;
+
+@Data
+public class CreateUserRequest {
+
+    @NotBlank(message = "Username cannot be empty")
+    private String username;
+
+    @Email(message = "Email must be valid")
+    @NotBlank(message = "Email cannot be empty")
+    private String email;
+
+    @NotBlank(message = "Password cannot be empty")
+    @ValidPassword
+    private String password;
+}
