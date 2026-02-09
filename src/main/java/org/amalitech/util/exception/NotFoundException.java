a814d1c9c3aac5943405b97a221a915c1d1d8877
Jacob Quaye <kofimave@gmail.com>
2026-02-09 07:15:51 +0000

    refactor: refactor renamed custom exception NotFoundException.java to ResourceNotFoundException

--- src/main/java/org/amalitech/util/exception/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package org.amalitech.util.exception;
-
-
-public class NotFoundException extends RuntimeException {
-
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/org/amalitech/util/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package org.amalitech.util.exception;
+
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
