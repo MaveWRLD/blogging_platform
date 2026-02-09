3667834586265cd72139bb2dc93736d71a4638b2
Jacob Quaye <kofimave@gmail.com>
2026-02-09 07:05:04 +0000

    feat: Global custom exception handling

---
+++ src/main/java/org/amalitech/util/exception/CustomExceptionHandler.java
@@ -0,0 +1,56 @@
+package org.amalitech.util.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(value = ResourceNotFoundException.class)
+    public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        Map<String, Object> body = exceptionBody(ex.getMessage(), request);
+
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationErrors(MethodArgumentNotValidException ex) {
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", new Date());
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(err ->
+                errors.put(err.getField(), err.getDefaultMessage())
+        );
+
+        body.put("errors", errors);
+        body.put("message", "Validation failed");
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<?> handleDatabaseException(DatabaseException ex, WebRequest request) {
+        Map<String, Object> body = exceptionBody(ex.getMessage(), request);
+
+
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private static Map<String, Object> exceptionBody(String exception, WebRequest request) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", new Date());
+        body.put("message", exception);
+        body.put("path", request.getDescription(false));
+        return body;
+    }
+}
