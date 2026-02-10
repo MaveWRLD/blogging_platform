fa52b9cbe4464758f7eefced48f6602bd957afdd
Jacob Quaye <kofimave@gmail.com>
2026-02-10 10:08:47 +0000

    feat: added HTTPStatus code to the response format

--- src/main/java/org/amalitech/dtos/ApiResponse.java
+++ src/main/java/org/amalitech/dtos/ApiResponse.java
@@ -2,23 +2,28 @@ package org.amalitech.dtos;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.http.HttpStatus;
 
 @Data
 @AllArgsConstructor
 public class ApiResponse<T> {
-    private String status;
+    private HttpStatus status;
     private String message;
     private T data;
 
     public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>("success", null, data);
+        return new ApiResponse<>(HttpStatus.OK, null, data);
     }
 
     public static <T> ApiResponse<T> success(T data, String message) {
-        return new ApiResponse<>("success", message, data);
+        return new ApiResponse<>(HttpStatus.OK, message, data);
+    }
+
+    public static <T> ApiResponse<T> success(HttpStatus status, T data, String message) {
+        return new ApiResponse<>(status, message, data);
     }
 
     public static <T> ApiResponse<T> error(String message) {
-        return new ApiResponse<>("error", message, null);
+        return new ApiResponse<>(HttpStatus.BAD_REQUEST, message, null);
     }
 }
