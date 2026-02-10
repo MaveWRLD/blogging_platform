6b4c80d925a28d965e83663af2dcd307653e7123
Jacob Quaye <kofimave@gmail.com>
2026-02-10 13:26:34 +0000

    feature: password validator

---
+++ src/main/java/org/amalitech/util/PasswordConstraintValidator.java
@@ -0,0 +1,19 @@
+package org.amalitech.util;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.amalitech.interfaces.ValidPassword;
+
+public class PasswordConstraintValidator implements ConstraintValidator<ValidPassword, String> {
+
+    @Override
+    public boolean isValid(String password, ConstraintValidatorContext context) {
+        if (password == null) return false;
+
+        return password.length() >= 8
+                && password.matches(".*[A-Z].*")
+                && password.matches(".*[a-z].*")
+                && password.matches(".*\\d.*")
+                && password.matches(".*[@#$%!].*");
+    }
+}
