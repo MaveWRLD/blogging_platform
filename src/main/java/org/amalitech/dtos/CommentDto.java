084b7b5612382496466a0eafd7e8e3a61fd6c17e
Jacob Quaye <kofimave@gmail.com>
2026-02-10 09:57:33 +0000

    feature: add constructors

--- src/main/java/org/amalitech/dtos/CommentDto.java
+++ src/main/java/org/amalitech/dtos/CommentDto.java
@@ -1,10 +1,14 @@
 package org.amalitech.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
-
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Builder
 public class CommentDto {
