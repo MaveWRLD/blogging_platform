86aef72952b417c7c203fd43b3c07be103a88d4b
Jacob Quaye <kofimave@gmail.com>
2026-02-15 11:05:44 +0000

    feat: enable caching

--- src/main/java/org/amalitech/AppLauncher.java
+++ src/main/java/org/amalitech/AppLauncher.java
@@ -2,8 +2,10 @@ package org.amalitech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class AppLauncher{
 
     public static void main(String[] args) {
