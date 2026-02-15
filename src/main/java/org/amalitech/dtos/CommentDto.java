629f824b4efa8ca4f0ebff964fbd3c6055847e99
Jacob Quaye <kofimave@gmail.com>
2026-02-15 05:10:23 +0000

    refactor: used instant in place of LocalDateTime

--- src/main/java/org/amalitech/dtos/CommentDto.java
+++ src/main/java/org/amalitech/dtos/CommentDto.java
@@ -5,7 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,5 +15,5 @@ public class CommentDto {
     private String id;
     private String username;
     private String body;
-    private LocalDateTime createdAt;
+    private Instant createdAt;
 }
