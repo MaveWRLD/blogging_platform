5f413df271dfd6f492f283fb84a7192774e4e04b
Jacob Quaye <kofimave@gmail.com>
2026-02-15 04:43:16 +0000

    refactor: delete manual database implementations

--- src/main/java/org/amalitech/interfaces/UserRoleRepository.java
+++
@@ -1,6 +0,0 @@
-package org.amalitech.interfaces;
-
-public interface UserRoleRepository {
-    void assignRoleToUser(int userId, int roleId);
-    void removeRoleFromUser(int userId, int roleId);
-}
