4e2d6ae9927d63620badb673ad4ce564dc9816e2
Jacob Quaye <kofimave@gmail.com>
2026-02-25 02:22:58 +0000

    refactor: clean up PostController by removing unused imports to enhance code readability

--- src/main/java/org/amalitech/controllers/PostController.java
+++ src/main/java/org/amalitech/controllers/PostController.java
@@ -1,6 +1,5 @@
 package org.amalitech.controllers;
 
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,13 +18,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 import java.util.List;
