c2b80953cde585cb0a515d78a74bda056ee80216
Jacob Quaye <kofimave@gmail.com>
2026-03-08 21:29:28 +0000

    feat: enabled scheduling for spring boot application

--- src/main/java/org/amalitech/AppLauncher.java
+++ src/main/java/org/amalitech/AppLauncher.java
@@ -3,8 +3,10 @@ package org.amalitech;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @EnableCaching
 public class AppLauncher{
 
